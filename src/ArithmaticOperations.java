154db1714d5811e51543d26af5855d57fa8a80cb
Tushar Savale <tusharsavale27@gmail.com>
2021-01-07 23:59:15 +0530

    Update code as per requested changes

--- src/ArithmaticOperations.java
+++ src/ArithmaticOperations.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 
+/*
+ * This function is to perform arithmatic operations
+ */
 public class ArithmaticOperations {
 
 	public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
     
-    //Taking inout from user
+    // Taking input from user
     System.out.println("Enter first number :");
     int num1 = scan.nextInt();
     System.out.println("Enter second number :");
     int num2 = scan.nextInt();
     
     
-    //Now Perform arithmatic operations
+    // Now Perform arithmatic operations
     
     int addition        = num1 + num2;
     int substraction    = num1 - num2;
     int multiplication  = num1 * num2;
     int division        = num1 / num2;
     
-    //now we print result to console
+    // now we print result to console
     
     System.out.println("Addition : "       + addition );
     System.out.println("Substraction : "   + substraction);
     System.out.println("Multiplication : " + multiplication);
     System.out.println("Division : "       + division);
-    
-
-	}
-
+    }
 }
